068b3ec279d2fd1841626b3dd4a16a4736e4d1b1
Md Shoaib Ahmed <[shoaibmehrab011@gmail.com]>
2021-10-07 16:08:41 +0600

    add projection

--- Startup.cs
+++ Startup.cs
@@ -33,7 +33,8 @@ namespace CommanderGQL
 
             services
                 .AddGraphQLServer()
-                .AddQueryType<Query>();
+                .AddQueryType<Query>()
+                .AddProjections();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
